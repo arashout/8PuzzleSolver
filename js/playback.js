c4412ce2c0c6abc99b426f037320c4acbb271043
arashout <arash.out@gmail.com>
2017-01-14 17:16:03 -0800

    Pressing previous now stops playback
    Next step is making css transition variable for more readable code

--- js/playback.js
+++ js/playback.js
@@ -1,17 +1,29 @@
-function nextMove(solObj, play){
+//Create global variables to pause/start playback
+var play = false;
+var nextMoveTimer;
+//Pick any tile to determine how long the setTimeout needs to wait for animations to finish
+var tileElement =  document.getElementsByClassName("tile")[0];
+var cssTransitionTime = tileElement.style.transitionDuration;
+function nextMove(solObj){
+    //Ensure we are not at the end of the solution
     if(solObj.i !== solObj.solutionBoards.length - 1){
         solObj.solutionBoards[solObj.i + 1].arrangeBlocks();
         solObj.i = solObj.i + 1;
     }
+    //If we are at the end, then don't consider anything else
     else{return;}
+    //If variable turn on in playMoves then continue to call nextMove
     if(play){
-        setTimeout(function() {nextMove(solObj, true);}, 500);
+        nextMoveTimer = setTimeout(function() {nextMove(solObj);}, 500);
     }
 }
 function playMoves(solObj){
-    nextMove(solObj, true);
+    play = true;
+    nextMove(solObj);
 }
 function prevMove(solObj){
+    play = false; //Pause playback
+    window.clearTimeout(nextMoveTimer); //Clear timeout so nextMove is not called again
     if(solObj.i !== 0){
         solObj.solutionBoards[solObj.i - 1].arrangeBlocks();
         solObj.i = solObj.i - 1;
