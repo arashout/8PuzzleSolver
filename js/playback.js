6438c866dd05b72379387ca4398bf21f780c5ade
arashout <arash.out@gmail.com>
2017-01-14 21:13:09 -0800

    Add variable for transition time so I can easily change css without breaking the animations!

--- js/playback.js
+++ js/playback.js
@@ -3,7 +3,10 @@ var play = false;
 var nextMoveTimer;
 //Pick any tile to determine how long the setTimeout needs to wait for animations to finish
 var tileElement =  document.getElementsByClassName("tile")[0];
-var cssTransitionTime = tileElement.style.transitionDuration;
+var cssTransitionTime = window.getComputedStyle(tileElement,null).getPropertyValue("transition-duration");
+//Has "s" seconds at the end need to truncate and convert to milliseconds
+cssTransitionTime = cssTransitionTime.slice(0,-1);
+cssTransitionTime = cssTransitionTime * 1000;
 function nextMove(solObj){
     //Ensure we are not at the end of the solution
     if(solObj.i !== solObj.solutionBoards.length - 1){
@@ -14,7 +17,7 @@ function nextMove(solObj){
     else{return;}
     //If variable turn on in playMoves then continue to call nextMove
     if(play){
-        nextMoveTimer = setTimeout(function() {nextMove(solObj);}, 500);
+        nextMoveTimer = setTimeout(function() {nextMove(solObj);}, cssTransitionTime);
     }
 }
 function playMoves(solObj){
